bd3afe395940f3d05758490f6d3b526bac238359
AnthonyM <tonymiyo132@gmail.com>
2021-09-24 21:03:18 -0500

    Create Problema4.java

---
+++ Lab1/Problema4.java
@@ -0,0 +1,28 @@
+//Problema 4
+class Main {
+  public static void main(String[] args) {
+    int a = 9, b = 5, c = 8;
+        
+    if (a > b && a > c) {
+      if (b > c) {
+        System.out.println(a + " " + b + " " + c);
+      } else {
+        System.out.println(a + " " + c + " " + b);
+      }
+     }
+    if (b > a && b > c) {
+      if (a > c) {
+        System.out.println(b + " " + a + " " + c);
+      } else {
+        System.out.println(b + " " + c + " " + a);
+      }
+     }
+    if (c > a && c > b) {
+      if (a > b) {
+        System.out.println(c + " " + a + " " + b);
+      } else {
+        System.out.println(c + " " + b + " " + a);
+      }
+    }
+  }
+}
